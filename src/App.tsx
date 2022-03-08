e1d08ed77fe156d9c98103fa70206e55996029bb
nsjteo <n.teomacdonald@gmail.com>
2022-03-08 03:09:29 -0800

    ShortUrl component and styles

--- src/App.tsx
+++ src/App.tsx
@@ -230,6 +230,54 @@ const Error = styled.p`
   margin-top: 4px;
 `;
 
+const ShortUrlContainer = styled.li`
+  list-style: none;
+  background: white;
+  border-radius: 5px;
+  padding-bottom: 1rem;
+  padding-top: 6px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 24px;
+`;
+
+const ShortUrlName = styled.p`
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 36px;
+  letter-spacing: 0.12px;
+  color: #34313d;
+  padding-bottom: 6px;
+  padding-left: 1rem;
+  border-bottom: 1px solid rgba(158, 154, 168, 0.25);
+`;
+
+const ShortUrl = styled.p`
+  padding-top: 6px;
+  padding-left: 1rem;
+  padding-bottom: 0.5rem;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 36px;
+  letter-spacing: 0.12px;
+  color: #2bd0d0;
+`;
+
+const CopyShortUrlButton = styled.button`
+  margin: 0 1rem;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: center;
+  color: #ffffff;
+  background: #2bd0d0;
+  border-radius: 5px;
+  border: none;
+  padding-top: 9px;
+  padding-bottom: 7px;
+  cursor: pointer;
+`;
+
 type shortLink = {
   url: string;
   name: string;
@@ -301,14 +349,13 @@ function App() {
         <Urls>
           {urls.map((url) => {
             return (
-              <li>
-                <p>{url.url}</p>
-                <p>{url.name}</p>
-              </li>
+              <ShortUrlContainer>
+                <ShortUrlName>{url.name}</ShortUrlName>
+                <ShortUrl>{url.url}</ShortUrl>
+                <CopyShortUrlButton>Copy</CopyShortUrlButton>
+              </ShortUrlContainer>
             );
           })}
-          <li>Url</li>
-          <li>Url</li>
         </Urls>
         <Details>
           <DetailsHeader>Advanced Statistics</DetailsHeader>
